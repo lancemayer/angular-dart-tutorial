125e0a00943bd5cab46b0cf7652f859fef9e11c2
Lance Klinger <lance.d.klinger@gmail.com>
2018-04-30 08:44:53 -0400

    Working on Routing. Next: Style the app.

--- lib/src/hero_list_component.dart
+++ lib/src/hero_list_component.dart
@@ -1,24 +1,28 @@
 import 'package:angular/angular.dart';
 import 'package:angular_forms/angular_forms.dart';
+import 'package:angular_router/angular_router.dart';
+
 import 'dart:async';
 import 'hero.dart';
-import 'hero_component.dart';
 import 'hero_service.dart';
+import 'route_paths.dart' as paths;
 
 @Component(
   selector: 'my-heroes',
   templateUrl: 'hero_list_component.html',
   styleUrls: ['hero_list_component.css'],
-  directives: [coreDirectives, formDirectives, HeroComponent],
+  directives: [coreDirectives, formDirectives],
   providers: [const ClassProvider(HeroService)],
+  pipes: [commonPipes],
 )
 class HeroListComponent implements OnInit {
   final HeroService _heroService;
-  HeroListComponent(this._heroService);
+  final Router _router;
   final title = 'Tour de Heroes';
-
   List<Hero> heroes;
   Hero selected;
+
+  HeroListComponent(this._heroService, this._router);
 
   void ngOnInit() => _getHeroes();
 
@@ -27,4 +31,10 @@ class HeroListComponent implements OnInit {
   Future<void> _getHeroes() async {
     heroes = await _heroService.getAll();
   }
+
+  String _heroUrl(int id) =>
+    paths.hero.toUrl(parameters: {paths.idParam: id.toString()});
+
+  Future<NavigationResult> gotoDetail() => 
+    _router.navigate(_heroUrl(selected.id));
 }
