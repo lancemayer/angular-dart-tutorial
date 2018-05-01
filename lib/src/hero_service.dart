00c766fac656fa53a03cf7e72804bf8b34e9215b
Lance Klinger <lance.d.klinger@gmail.com>
2018-05-01 19:47:44 -0400

    Working on HTTP. Next: HTTP Future

--- lib/src/hero_service.dart
+++ lib/src/hero_service.dart
@@ -1,16 +1,47 @@
-import 'package:angular/angular.dart';
 import 'dart:async';
+import 'dart:convert';
+
+import 'package:angular/angular.dart';
+import 'package:http/http.dart';
+
 import 'hero.dart';
-import 'mock_heroes.dart';
+// import 'mock_heroes.dart';
 
 @Injectable()
 class HeroService {
   Future<Hero> get(int id) async =>
     (await getAll()).firstWhere((hero) => hero.id == id);
-  
-  Future<List<Hero>> getAll() async => mockHeroes;
 
-  Future<List<Hero>> getAllSlowly() {
-    return new Future.delayed(const Duration(seconds: 2), getAll);
+  static const _heroesUrl = 'api/heroes';
+
+  final Client _http;
+
+  HeroService(this._http);
+  
+  // Future<List<Hero>> getAll() async => mockHeroes;
+  Future<List<Hero>> getAll() async {
+    try{
+      final response = await _http.get(_heroesUrl);
+      final heroes = _extractData(response)
+        .map((value) => new Hero.fromJson(value))
+        .toList();
+      
+      return heroes;
+    } catch (e) {
+      throw _handleError(e);
+    }
+  }
+
+  dynamic _extractData(Response resp) => json.decode(resp.body)['data'];
+
+  Exception _handleError(dynamic e) {
+    print(e);
+    return new Exception('Server error; cause: $e');
   }
 }
+
+
+  // Future<List<Hero>> getAllSlowly() {
+  //   return new Future.delayed(const Duration(seconds: 2), getAll);
+  // }
+// }
--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,17 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:angular_app/app_component.template.dart' as ng;
+// import 'package:http/browser_client.dart';
+import 'package:angular_app/in_memory_data_service.dart';
+import 'package:http/http.dart';
 
 import 'main.template.dart' as self;
 
-@GenerateInjector(
+@GenerateInjector([
   routerProvidersHash,
-)
+  // const ClassProvider(BrowserClient),
+  const ClassProvider(Client, useClass: InMemoryDataService),
+])
 final InjectorFactory injector = self.injector$Injector;
 
 void main() {
